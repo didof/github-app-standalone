ce0dba1d15ca2a0217f75bbdd026cd604c768106
didof <didonato.fr@gmail.com>
2022-04-23 16:33:33 +0200

    feature: handle github oauth authentication
    Signed-off-by: didof <didonato.fr@gmail.com>

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
+require("dotenv").config();
 const path = require("path");
 const server = require("fastify")();
+const axios = require("axios");
 
 server.register(require("fastify-static"), {
   root: path.join(__dirname, "public"),
   prefix: "/public/",
+});
+
+server.get("/oauth/github/login/callback", async (request, reply) => {
+  const { code } = request.query;
+
+  const exchangeURL = new URL("login/oauth/access_token", "https://github.com");
+  exchangeURL.searchParams.set("client_id", process.env.CLIENT_ID);
+  exchangeURL.searchParams.set("client_secret", process.env.CLIENT_SECRET);
+  exchangeURL.searchParams.set("code", code);
+
+  const response = await axios.post(exchangeURL.toString(), null, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+
+  const { access_token } = response.data;
+
+  const redirectionURL = new URL("welcome", "http://localhost:3000");
+  redirectionURL.searchParams.set("access_token", access_token);
+
+  reply.status(302).header("Location", redirectionURL).send();
+});
+
+server.get("/welcome", (request, reply) => {
+  return reply.sendFile("welcome.html");
 });
 
 server.get("/", (request, reply) => {
